cd2e1f5934281e2c099074014195e4bba8cc45f1
ryanhtanto <ryanherrtanto@gmail.com>
2023-02-26 13:36:38 +0700

    fixing

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -7,7 +7,7 @@ function useInput(defaultValue = '') {
     setValue(target.value);
   }
 
-  return [value, handleValueChange];
+  return [value, handleValueChange, setValue];
 }
 
 export default useInput;

e770bfce05d80fd0045797a56c65b06a2db1fdfa
ryanhtanto <ryanherrtanto@gmail.com>
2023-02-26 20:42:26 +0700

    styled component

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import LoadingBar from 'react-redux-loading-bar';
+import LoadingBarStyled from './styled/LoadingBarStyled';
 
 function Loading() {
   return (
-    <div className="loading">
+    <LoadingBarStyled>
       <LoadingBar />
-    </div>
+    </LoadingBarStyled>
   );
 }
 
